65b4d34bf0a7a172633f2987dd63d00b75f2a0e8
Tiago Ribeiro de Souza <tiago.astro@gmail.com>
2017-02-18 15:19:47 -0300

    WavePlate was not properly defined.

--- chimera_t80cam/instruments/fsupolarimeterwaveplate.py
+++ chimera_t80cam/instruments/fsupolarimeterwaveplate.py
@@ -4,12 +4,12 @@ from chimera_t80cam.instruments.ebox.fsupolarimeter.fsupolarimeter import Polari
 log = logging.Logger(__name__)
 
 
-class FSUPolarimeterFilterWheel(PolarimeterWheelBase):
+class FSUPolarimeterWavePlate(PolarimeterWheelBase):
     def __init__(self):
         PolarimeterWheelBase.__init__(self)
         self["device"] = None
-        self['filter_wheel_model'] = "Fake Polarimeter Filter Wheel"
-        self["filters"] = "CLEAR B V R I"
+        self['filter_wheel_model'] = "Fake Polarimeter Wave Plate Wheel"
+        self["filters"] = "0.0 22.5 45.0 67.5 90.0 112.5 135.0 157.5 180.0 202.5 225.0 247.5 270.0 292.5 315.0 337.5"
         self["id"] = 2
 
     # def getMetadata(self, request):
